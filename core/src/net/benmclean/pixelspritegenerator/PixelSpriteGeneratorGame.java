2a5f1a52359db9078d3f2dd038ba7c2ec3366291
Benjamin McLean <mclean.ben@gmail.com>
2016-03-21 23:53:14 -0500

    added a pixmap

--- core/src/net/benmclean/pixelspritegenerator/PixelSpriteGeneratorGame.java
+++ core/src/net/benmclean/pixelspritegenerator/PixelSpriteGeneratorGame.java
@@ -3,17 +3,24 @@ package net.benmclean.pixelspritegenerator;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class PixelSpriteGeneratorGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Texture pixmaptex;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		Pixmap pixmap = new Pixmap( 64, 64, Pixmap.Format.RGBA8888 );
+		pixmap.setColor( 0, 1, 0, 0.75f );
+		pixmap.fillCircle( 32, 32, 32 );
+		pixmaptex = new Texture( pixmap );
+		pixmap.dispose();
 	}
 
 	@Override
@@ -22,6 +29,7 @@ public class PixelSpriteGeneratorGame extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
+        batch.draw(pixmaptex, 300, 300);
 		batch.end();
 	}
 }
