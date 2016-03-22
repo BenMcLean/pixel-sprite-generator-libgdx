c503276db55bb04affb32ea025baa99501c9d260
Benjamin McLean <mclean.ben@gmail.com>
2016-03-22 14:45:22 -0500

    fixed UI scaling so this works on both desktop and android

--- core/src/net/benmclean/pixelspritegenerator/PixelSpriteGeneratorGame.java
+++ core/src/net/benmclean/pixelspritegenerator/PixelSpriteGeneratorGame.java
@@ -10,6 +10,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.kotcrab.vis.ui.VisUI;
 import com.kotcrab.vis.ui.widget.VisTable;
 import com.kotcrab.vis.ui.widget.VisTextButton;
@@ -69,7 +70,7 @@ public class PixelSpriteGeneratorGame extends ApplicationAdapter {
 	public void create () {
         VisUI.load();
         batch = new SpriteBatch();
-        stage = new Stage();
+        stage = new Stage(new FitViewport(640, 480));
         Gdx.input.setInputProcessor(stage);
 
         table = new VisTable();
@@ -118,7 +119,7 @@ public class PixelSpriteGeneratorGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0.5f, 0.5f, 0.5f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-        final int scale = 32;
+        final float scale = (float)Math.sqrt(Gdx.graphics.getWidth() * Gdx.graphics.getHeight()) * 0.05f;
         batch.draw(pixmaptex, scale, scale, pixmaptex.getHeight() * scale, pixmaptex.getWidth() * scale);
 		batch.end();
         stage.act(Gdx.graphics.getDeltaTime());
