00da5dc2f8d8f27bc2e413cd1384db9314316b88
Benjamin McLean <mclean.ben@gmail.com>
2016-03-22 11:03:54 -0500

    added timer based seed, and scaled up sprite

--- core/src/net/benmclean/pixelspritegenerator/PixelSpriteGeneratorGame.java
+++ core/src/net/benmclean/pixelspritegenerator/PixelSpriteGeneratorGame.java
@@ -2,10 +2,10 @@ package net.benmclean.pixelspritegenerator;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import net.benmclean.pixelspritegenerator.pixelspritegenerator.Mask;
 import net.benmclean.pixelspritegenerator.pixelspritegenerator.Sprite;
@@ -17,6 +17,7 @@ public class PixelSpriteGeneratorGame extends ApplicationAdapter {
 	
 	@Override
 	public void create () {
+        int SEED = (int)System.currentTimeMillis();
         Sprite sprite = new Sprite(12, 12, new Mask(new int[]{
                 0, 0, 0, 0, 0, 0,
                 0, 0, 0, 0, 1, 1,
@@ -30,7 +31,7 @@ public class PixelSpriteGeneratorGame extends ApplicationAdapter {
                 0, 1, 1, 1, 1,-1,
                 0, 0, 0, 1, 1, 1,
                 0, 0, 0, 0, 0, 0
-        }, 6, 12, true, false), true, 0.3, 0.2, 0.3, 0.5, 0);
+        }, 6, 12, true, false), true, 0.3, 0.2, 0.3, 0.5, SEED);
 
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
@@ -58,6 +59,7 @@ public class PixelSpriteGeneratorGame extends ApplicationAdapter {
             }
 
 		pixmaptex = new Texture( pixmap );
+        pixmaptex.setFilter(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest);
 		pixmap.dispose();
 	}
 
@@ -66,8 +68,8 @@ public class PixelSpriteGeneratorGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
-        batch.draw(pixmaptex, 300, 300);
+		//batch.draw(img, 0, 0);
+        batch.draw(pixmaptex, 16, 16, pixmaptex.getHeight() * 16, pixmaptex.getWidth() * 16);
 		batch.end();
 	}
 }
